fc63f81eb8fd6397582e305aacc41a6840927917
kunitaya <tetsuya@kunitaya.info>
2017-04-28 19:19:14 +0900

    Corresponds to the first automatic installation of Wordpress

---
+++ startup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# function: install wp-cli
+function install-cli () {
+  cd /usr/local/bin
+  curl -sO https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+  chmod +x wp-cli.phar
+  ln -s wp-cli.phar wp-cli
+}
+
+# download wordpress
+if [ ! -z `find ${DOCROOT} -maxdepth 0 -type d -empty` ]; then
+  [ `which wp-cli` ]|| install-cli
+  wp-cli --allow-root core download --locale=${WP_LOCALE} --path=${DOCROOT}
+fi
+
+# create config file
+if [[ -e ${DOCROOT}/wp-config.php && ${CREATE_CONF} -eq 1 ]]; then
+  rm -f ${DOCROOT}/wp-config.php
+fi
+if [[ ! -e ${DOCROOT}/wp-config.php ]]; then
+  [ `which wp-cli` ]|| install-cli
+  [ `which mysql` ]|| yum -y install mariadb
+  wp-cli --allow-root core config --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${MYSQL_HOST} --path=${DOCROOT}
+fi
+
+# change owner
+chown -R apache. ${DOCROOT}
+
+# start httpd
+/usr/sbin/apachectl -D FOREGROUND
